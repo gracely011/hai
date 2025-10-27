8d90e9ed98008f3c1b2ee27136ae0cced2b47078
gracely011 <petrusperdana4@gmail.com>
2025-10-27 11:10:26 +0700

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,6 +1,5 @@
-const announcementBarHTML = `
-
-`;
+// Kosongkan sesuai permintaan Anda
+const announcementBarHTML = ``;
 
 const defaultNavbarHTML = `
 <header class="ud-header">
@@ -8,8 +7,8 @@ const defaultNavbarHTML = `
     <div class="row">
       <div class="col-lg-12">
         <nav class="navbar navbar-expand-lg">
-          <a class="navbar-brand" href="https://gracely011.github.io/hai/index.html">
-            <img src="https://groupy.id/assets/images/logo/groupy_mobile_white.png" alt="Logo" id="logo" />
+          <a class="navbar-brand" href="index.html">
+            <img src="assets/images/logo/groupy_mobile_white.png" alt="Logo" id="logo" />
           </a>
           <button class="navbar-toggler">
             <span class="toggler-icon"></span><span class="toggler-icon"></span><span class="toggler-icon"></span>
@@ -24,8 +23,8 @@ const defaultNavbarHTML = `
             </ul>
           </div>
           <div class="navbar-btn">
-            <a href="https://gracely011.github.io/hai/login.html" class="ud-main-btn ud-login-btn">Log In</a>
-            <a href="https://gracely011.github.io/hai/signup.html" class="ud-main-btn ud-white-btn">Sign Up</a>
+            <a href="login.html" class="ud-main-btn ud-login-btn">Log In</a>
+            <a href="signup.html" class="ud-main-btn ud-white-btn">Sign Up</a>
           </div>
         </nav>
       </div>
@@ -41,7 +40,7 @@ const loggedInNavbarHTML = (userName) => `
       <div class="col-lg-12">
         <nav class="navbar navbar-expand-lg">
           <a class="navbar-brand" href="index.html">
-             <img src="https://groupy.id/assets/images/logo/groupy_mobile_white.png" alt="Logo" id="logo" />
+             <img src="assets/images/logo/groupy_mobile_white.png" alt="Logo" id="logo" />
           </a>
           <button class="navbar-toggler">
             <span class="toggler-icon"></span><span class="toggler-icon"></span><span class="toggler-icon"></span>
@@ -68,17 +67,17 @@ const loggedInNavbarHTML = (userName) => `
 
 const footerHTML = `
 <footer class="ud-footer" data-wow-delay=".15s">
-    <div class="shape shape-1"><img src="https://groupy.id/assets/images/footer/leftshape.png" alt="shape"/></div>
-    <div class="shape shape-2"><img src="https://groupy.id/assets/images/footer/dotted-shape.svg" alt="shape"/></div>
-    <div class="shape shape-3"><img src="https://groupy.id/assets/images/footer/rightshape.png" alt="shape"/></div>
+    <div class="shape shape-1"><img src="assets/images/footer/leftshape.png" alt="shape"/></div>
+    <div class="shape shape-2"><img src="assets/images/footer/dotted-shape.svg" alt="shape"/></div>
+    <div class="shape shape-3"><img src="assets/images/footer/rightshape.png" alt="shape"/></div>
     <div class="ud-footer-widgets">
         <div class="container">
             <div class="row">
-                <div class="col-xl-3 col-lg-4 col-md-6"><div class="ud-widget"><a href="index.html" class="ud-footer-logo"><img src="https://groupy.id/assets/images/logo/groupy_white.png" alt="logo"/></a><p class="ud-widget-desc">Unlock Premium Together</p><ul class="ud-widget-socials"><li><a href="#"><i class="fa-brands fa-instagram"></i></a></li><li><a href="#"><i class="fa-brands fa-discord"></i></a></li></ul></div></div>
+                <div class="col-xl-3 col-lg-4 col-md-6"><div class="ud-widget"><a href="index.html" class="ud-footer-logo"><img src="assets/images/logo/groupy_white.png" alt="logo"/></a><p class="ud-widget-desc">Unlock Premium Together</p><ul class="ud-widget-socials"><li><a href="#"><i class="fa-brands fa-instagram"></i></a></li><li><a href="#"><i class="fa-brands fa-discord"></i></a></li></ul></div></div>
                 <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6"><div class="ud-widget"><h5 class="ud-widget-title">About</h5><ul class="ud-widget-links"><li><a href="index.html#features">Features</a></li><li><a href="index.html#about">About</a></li><li><a href="index.html#pricing">Pricing</a></li><li><a href="index.html#services">Services</a></li></ul></div></div>
-                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6"><div class="ud-widget"><h5 class="ud-widget-title">Dashboard</h5><ul class="ud-widget-links"><li><a href="https://gracely011.github.io/hai/dashboard.html">View dashboard <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li><li><a href="#">Purchase premium <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li></ul></div></div>
-                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6"><div class="ud-widget"><h5 class="ud-widget-title">Extension</h5><ul class="ud-widget-links"><li><a href="https://gracely011.github.io/hai/dashboard.html">Download extension <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li></ul></div></div>
-                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-10"><div class="ud-widget"><h5 class="ud-widget-title">Partners</h5><ul class="ud-widget-brands"><li><a href="#" rel="nofollow noopener" target="_blank"><img src="https://tripay.co.id/new-template/images/logo-dark.png" alt="TriPay"></a><a href="#" rel="nofollow noopener" target="_blank"><img src="https://companieslogo.com/img/orig/NET_BIG.D-52893f5e.png" alt="Cloudflare"></a></li></ul></div></div>
+                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6"><div class="ud-widget"><h5 class="ud-widget-title">Dashboard</h5><ul class="ud-widget-links"><li><a href="dashboard.html">View dashboard <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li><li><a href="#">Purchase premium <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li></ul></div></div>
+                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6"><div class="ud-widget"><h5 class="ud-widget-title">Extension</h5><ul class="ud-widget-links"><li><a href="dashboard.html">Download extension <i class="fa-solid fa-arrow-up-right-from-square"></i></a></li></ul></div></div>
+                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-10"><div class="ud-widget"><h5 class="ud-widget-title">Partners</h5><ul class="ud-widget-brands"><li><a href="#" rel="nofollow noopener" target="_blank"><img src="httpsid/new-template/images/logo-dark.png" alt="TriPay"></a><a href="#" rel="nofollow noopener" target="_blank"><img src="https://companieslogo.com/img/orig/NET_BIG.D-52893f5e.png" alt="Cloudflare"></a></li></ul></div></div>
             </div>
         </div>
     </div>
@@ -96,27 +95,28 @@ const footerHTML = `
 const backToTopHTML = `<a href="javascript:void(0)" class="back-to-top"><i class="fa-solid fa-arrow-up"></i></a>`;
 
 /**
- * [FUNGSI DIMODIFIKASI]
- * Fungsi ini sekarang menargetkan tombol "Purchase" dan "Watch Demo"
- * dari file index.html Groupy yang asli.
+ * [FUNGSI DIPERBARUI]
+ * Fungsi ini sekarang menargetkan tombol di index.html Groupy yang asli.
  */
 function modifyIndexPageContent() {
     const path = window.location.pathname;
-    const isIndexPage = path.endsWith('/') || path.endsWith('index.html');
+    const isIndexPage = path.endsWith('/') || path.endsWith('index.html') || path.endsWith('hai/');
 
     if (!isIndexPage) {
         return;
     }
 
     if (typeof isAuthenticated === 'function' && isAuthenticated()) {
-        // Cari tombol berdasarkan kelas uniknya
-        const purchaseButton = document.querySelector('.ud-hero-buttons .ud-white-btn');
-        const demoButton = document.querySelector('.ud-hero-buttons .ud-link-btn');
+        // Cari tombol "Purchase" dan "Watch Demo" di hero section
+        const purchaseButton = document.querySelector('#home .ud-hero-buttons .ud-white-btn');
+        const demoButton = document.querySelector('#home .ud-hero-buttons .ud-link-btn');
 
         // Ubah tombol "Purchase" menjadi "Go to Dashboard"
         if (purchaseButton) {
             purchaseButton.textContent = 'Go to Dashboard';
             purchaseButton.href = 'dashboard.html';
+            // Hapus target="_blank" jika ada
+            purchaseButton.removeAttribute('target');
         }
 
         // Sembunyikan tombol "Watch Demo"
@@ -146,10 +146,12 @@ function loadLayout() {
         }
     }
 
+    // Panggil initializeScripts() dari scripts.js SETELAH layout dimuat
     if (typeof initializeScripts === 'function') {
       initializeScripts();
     }
     
+    // Panggil fungsi untuk memodifikasi index.html
     modifyIndexPageContent();
 }
 
