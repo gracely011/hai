2c3d57956afaa6fe408b4e6dfd89daaa9ca73813
petrusperdana1 <petrusperdanasiahaan@gmail.com>
2026-06-16 20:28:37 +0700

    Update aturhonma.js

--- aturhonma.js
+++ aturhonma.js
@@ -184,7 +184,7 @@
     purchase: "https://draft.gracely.my.id/premium.html",
     onInstallHomepage: "https://www.instagram.com/petrusperdana1/",
     onInstallSocial: "https://draft.gracely.my.id/extension.html",
-    onUninstall: "https://petrussiahaan.blogspot.com/p/ruang-syahdu.html",
+    onUninstall: "https://draft.gracely.my.id/p/ruang-syahdu.html",
     onGuardMissing: "https://draft.gracely.my.id/guardrequired.html",
     onLogoutBlock: "https://draft.gracely.my.id/blocked.html",
     onLoggedOutRedirect: "https://draft.gracely.my.id/login.html",
